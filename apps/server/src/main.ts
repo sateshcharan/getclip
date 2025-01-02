ebe533630ab69419397b055463c9a2782d6342e8
sateshcharan <sateshcharans@gmail.com>
2025-01-02 07:58:47 +0000

    removed prisma

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -6,7 +6,7 @@
 import express from 'express';
 import * as path from 'path';
 import cors from 'cors';
-import prisma from './prisma.service';
+// import prisma from './prisma.service';
 
 const app = express();
 app.use(cors());
@@ -15,8 +15,8 @@ app.use(cors());
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/api', async (req, res) => {
-  const users = await prisma.user.findMany();
-  res.send({ message: users[0] });
+  // const users = await prisma.user.findMany();
+  res.send({ message: "users[0]" });
 });
 
 const port = process.env.PORT || 3333;
