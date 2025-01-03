7559bbc019fd467220bc640086e27b63de72f2d8
sateshcharan <sateshcharans@gmail.com>
2025-01-03 12:49:53 +0000

    update workflows

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -7,16 +7,27 @@ import express from 'express';
 import * as path from 'path';
 import cors from 'cors';
 // import prisma from './prisma.service';
+// import { supabase } from './supabase';
 
 const app = express();
 app.use(cors());
 
-
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/api', async (req, res) => {
+  //local
   // const users = await prisma.user.findMany();
-  res.send({ message: "users[0]" });
+  // res.send({ message: users[0] });
+
+  // supabase
+  // const { data, error } = await supabase.from('User').select('*');
+  // if (error) {
+  //   return res.status(400).json({ error: error.message });
+  // }
+  // res.send({ message: data[0] });
+
+  //test
+  res.send({ message: 'Hello API' });
 });
 
 const port = process.env.PORT || 3333;
